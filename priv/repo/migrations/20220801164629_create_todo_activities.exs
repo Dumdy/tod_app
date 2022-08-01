9bc573da3348b74d705a0ff03d0f669e2c55e830
dumdy <dumadibakor@gmail.com>
2022-08-01 20:48:17 +0100

    accounts context test done

---
+++ priv/repo/migrations/20220801164629_create_todo_activities.exs
@@ -0,0 +1,14 @@
+defmodule Todo.Repo.Migrations.CreateTodoActivities do
+  use Ecto.Migration
+
+  def change do
+    create table(:todo_activities) do
+      add :activity, :string
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:todo_activities, [:user_id])
+  end
+end
